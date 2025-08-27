32efb86f3619ddce8e517006ebd4780b750cd63f
doriankablar <dorian.kablar@true-north.hr>
2025-08-27 15:59:24 +0200

    add profiles

---
+++ lab3/src/main/java/tn/pbz/educ/lab3/controller/ProfileController.java
@@ -0,0 +1,18 @@
+package tn.pbz.educ.lab3.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tn.pbz.educ.lab3.service.ProfileService;
+
+@RestController
+@AllArgsConstructor
+public class ProfileController {
+
+  private final ProfileService profileService;
+
+  @GetMapping("/api/profile")
+  public String getProfileMessage() {
+    return profileService.getProfileMessage();
+  }
+}
--- lab3/src/main/java/tn/pbz/educ/lab3/service/impl/JsonProfileServiceImpl.java
+++ lab3/src/main/java/tn/pbz/educ/lab3/service/impl/JsonProfileServiceImpl.java
@@ -1,8 +1,10 @@
 package tn.pbz.educ.lab3.service.impl;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 import tn.pbz.educ.lab3.service.ProfileService;
 
+@Service
 @Profile("json-logs")
 public class JsonProfileServiceImpl implements ProfileService {
 
