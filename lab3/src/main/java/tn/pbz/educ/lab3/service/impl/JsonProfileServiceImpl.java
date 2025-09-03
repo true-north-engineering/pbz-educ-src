b97e50d1cdbe32fcdc00387278b92e7517a879f9
pmiseta <paula.miseta.guest@pbz.hr>
2025-09-03 13:10:15 +0200

    Added profiles

---
+++ lab3/src/main/java/tn/pbz/educ/lab3/service/impl/JsonProfileServiceImpl.java
@@ -0,0 +1,15 @@
+package tn.pbz.educ.lab3.service.impl;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import tn.pbz.educ.lab3.service.ProfileService;
+
+@Service
+@Profile("json-logs")
+public class JsonProfileServiceImpl implements ProfileService {
+
+    @Override
+    public String getProfileMessage() {
+        return "JSON profile message";
+    }
+}
